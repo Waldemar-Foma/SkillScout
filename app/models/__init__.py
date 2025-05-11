0fbc0d6a9d91fa407f826ec6cc4eceb982594c24
Foma <104798936+Waldemar-Foma@users.noreply.github.com>
2025-05-12 00:19:53 +0200

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .candidate import CandidateProfile
-from .employer import EmployerProfile
-from .vacancy import Vacancy
-
-__all__ = ['User', 'CandidateProfile', 'EmployerProfile', 'Vacancy']
